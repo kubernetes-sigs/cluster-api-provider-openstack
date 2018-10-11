88828cf5fe012e34776c6192548a5f5b6aa004bb
Jason Murray <jason@chaosaffe.io>
2018-10-11 16:16:37 +0200

    Fix openstackprovierconfig registration (#47)

--- pkg/apis/openstackproviderconfig/v1alpha1/register.go
+++ pkg/apis/openstackproviderconfig/v1alpha1/register.go
@@ -25,7 +25,7 @@ const GroupName = "openstackproviderconfig"
 
 var (
 	// SchemeGroupVersion is group version used to register these objects
-	SchemeGroupVersion = schema.GroupVersion{Group: "gceproviderconfig.k8s.io", Version: "v1alpha1"}
+	SchemeGroupVersion = schema.GroupVersion{Group: "openstackproviderconfig.k8s.io", Version: "v1alpha1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
 	SchemeBuilder = &scheme.Builder{GroupVersion: SchemeGroupVersion}
