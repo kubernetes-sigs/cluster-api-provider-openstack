237e4d6e297e9e368d31ef6a704e4d8136b7d0f1
Flavio Percoco <flaper87@gmail.com>
2018-11-14 12:25:50 +0100

    Rename the machine actuator constructor (#100)

--- pkg/controller/add_machine.go
+++ pkg/controller/add_machine.go
@@ -25,7 +25,7 @@ import (
 func init() {
 	// AddToManagerFuncs is a list of functions to create controllers and add them to a manager.
 	AddToManagerFuncs = append(AddToManagerFuncs, func(m manager.Manager) error {
-		machineActuator, err := ocm.NewMachineActuator(m.GetClient(), m.GetScheme())
+		machineActuator, err := ocm.NewActuator(m.GetClient(), m.GetScheme())
 		if err != nil {
 			return err
 		}
