263b3f3645b84b3871a1131ea6ff1fdc735bcd13
Cyrine Jabri <cyrine.jabri@innovo-cloud.de>
2018-12-10 16:40:34 +0100

    Title: Replace glog with klog #99

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -21,7 +21,6 @@ import (
 	"log"
 	"time"
 
-	"github.com/golang/glog"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 	"k8s.io/klog"
@@ -35,6 +34,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/runtime/signals"
 )
 
+// RootCmd manager
 var RootCmd = &cobra.Command{
 	Use:   "manager",
 	Short: "Cluster API provider OpenStack manager",
@@ -92,29 +92,29 @@ func main() {
 	// Get a config to talk to the apiserver
 	cfg, err := config.GetConfig()
 	if err != nil {
-		glog.Fatal(err)
+		klog.Fatal(err)
 	}
 
 	// Create a new Cmd to provide shared dependencies and start components
 	mgr, err := manager.New(cfg, manager.Options{})
 	if err != nil {
-		glog.Fatal(err)
+		klog.Fatal(err)
 	}
 
-	glog.Infof("Initializing Dependencies.")
+	klog.Infof("Initializing Dependencies.")
 
 	// Setup Scheme for all resources
 	if err := apis.AddToScheme(mgr.GetScheme()); err != nil {
-		glog.Fatal(err)
+		klog.Fatal(err)
 	}
 
 	if err := clusterapis.AddToScheme(mgr.GetScheme()); err != nil {
-		glog.Fatal(err)
+		klog.Fatal(err)
 	}
 
 	// Setup all Controllers
 	if err := controller.AddToManager(mgr); err != nil {
-		glog.Fatal(err)
+		klog.Fatal(err)
 	}
 
 	log.Printf("Starting the Cmd.")
