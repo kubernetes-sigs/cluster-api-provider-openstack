f63a80d43252d03d5e1ebc07f94251fc44648785
jichenjc <jichenjc@cn.ibm.com>
2019-05-15 01:44:54 +0000

    Add Record of instance action
    instance create and delete can be have some events so we can record
    them and for audit purpose.

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -25,6 +25,7 @@ import (
 	"k8s.io/klog"
 	"sigs.k8s.io/cluster-api-provider-openstack/pkg/apis"
 	"sigs.k8s.io/cluster-api-provider-openstack/pkg/controller"
+	"sigs.k8s.io/cluster-api-provider-openstack/pkg/record"
 	clusterapis "sigs.k8s.io/cluster-api/pkg/apis"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
@@ -70,6 +71,7 @@ func main() {
 		klog.Fatal(err)
 	}
 
+	record.InitFromRecorder(mgr.GetRecorder("openstack-controller"))
 	klog.Infof("Initializing Dependencies.")
 
 	// Setup Scheme for all resources
