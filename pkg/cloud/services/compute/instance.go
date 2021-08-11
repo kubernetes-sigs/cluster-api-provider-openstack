66684ed9905cc5f498ae6830d9c91297a9660505
zhangdb-git <59427860+zhangdb-git@users.noreply.github.com>
2021-08-11 16:18:22 +0800

    Update pkg/cloud/services/compute/instance.go
    Co-authored-by: tobiasgiese <tobiasgiese@gmail.com>

--- pkg/cloud/services/compute/instance.go
+++ pkg/cloud/services/compute/instance.go
@@ -277,7 +277,7 @@ func (s *Service) createInstance(eventObject runtime.Object, clusterName string,
 			return false, err
 		}
 		if createdInstance.State == infrav1.InstanceStateError {
-			return false, fmt.Errorf("openstack instance %s go error.", createdInstance.ID)
+			return false, fmt.Errorf("error creating OpenStack instance %s, status changed to error", createdInstance.ID)
 		}
 		return createdInstance.State == infrav1.InstanceStateActive, nil
 	})
