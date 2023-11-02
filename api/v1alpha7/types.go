323abadedd1c884cb37b345c3c6507736aaeb707
Stephen Finucane <stephenfin@redhat.com>
2023-11-02 15:37:04 +0000

    Add OpenStackMachineSpec.RootVolume.UseMachineAZ

--- api/v1alpha7/types.go
+++ api/v1alpha7/types.go
@@ -168,6 +168,12 @@ type RootVolume struct {
 	// AvailabilityZone specifies the Cinder Availability Zone to use for the root volume.
 	// Leave empty to allow Cinder to schedule the volume without an availability zone.
 	AvailabilityZone string `json:"availabilityZone,omitempty"`
+
+	// UseMachineAZ specifies that the root volume should be created in a cinder
+	// availability zone with the same name as the nova availability zone of the server,
+	// if any.
+	// It is an error to specify both AvailabilityZone and UseMachineAZ.
+	UseMachineAZ bool `json:"useMachineAZ,omitempty"`
 }
 
 // BlockDeviceStorage is the storage type of a block device to create and
@@ -202,6 +208,13 @@ type BlockDeviceVolume struct {
 	// further information.
 	// +optional
 	AvailabilityZone string `json:"availabilityZone,omitempty"`
+
+	// UseMachineAZ specifies that the root volume should be created in a cinder
+	// availability zone with the same name as the nova availability zone of the server,
+	// if any.
+	// It is an error to specify both AvailabilityZone and UseMachineAZ.
+	// +optional
+	UseMachineAZ bool `json:"useMachineAZ,omitempty"`
 }
 
 // AdditionalBlockDevice is a block device to attach to the server.
