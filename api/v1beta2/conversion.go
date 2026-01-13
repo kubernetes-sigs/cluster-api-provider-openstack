4d97601422fe3c444c20265f39e6ab5c54ad3c6c
Bharath Nallapeta <nr.bharath97@gmail.com>
2026-01-13 16:18:50 +0530

    feat: Add api/v1beta2 types with CAPI v1beta2 contract
    Signed-off-by: Bharath Nallapeta <nr.bharath97@gmail.com>

---
+++ api/v1beta2/conversion.go
@@ -0,0 +1,21 @@
+/*
+Copyright 2026 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1beta2
+
+// NOTE: v1beta2 will become the Hub in a future PR.
+// For now, v1beta1 remains the Hub to avoid breaking changes.
+// Conversion webhooks will be implemented in PR #2.
